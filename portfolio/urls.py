8bc3a29375b0f57732ce354342bcab523b98545a
Chali Musonda <mcmusonda@gmail.com>
2026-01-18 04:16:07 +0200

    Course model

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path("courses/", include("courses.urls")),
     path("admin/", admin.site.urls),
 ]
+    
