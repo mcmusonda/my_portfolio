5466d9ec92349b07e43c0487133cf2c8a4ab91eb
Chali Musonda <mcmusonda@gmail.com>
2026-01-18 05:24:08 +0200

    Displaying data from db

--- courses/views.py
+++ courses/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from . models import Course
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'courses/index.html')
+    courses = Course.objects
+    return render(request, 'courses/index.html', {'courses': courses})
